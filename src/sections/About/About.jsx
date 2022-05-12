93f6d3ae4832c9843eb5341fa2f443543a5783c2
jcanort <jcanort@gmail.com>
2022-05-12 10:04:50 +0200

    Fixing bug in class app__profile-item className

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -2,46 +2,42 @@ import React, { useState, useEffect } from 'react';
 import './About.scss';
 
 //Assets
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { urlFor, client } from '../../client';
+import { Images } from '../../constants';
 import { motion } from 'framer-motion';
 
+const abouts = [
+    {title: 'Web Development', description: 'I am a good webdeveloper', imgUrl: Images.about01},
+    {title: 'Front Development', description: 'I am a good webdeveloper', imgUrl: Images.about02},
+    {title: 'Back Development', description: 'I am a good webdeveloper', imgUrl: Images.about03},
+    {title: 'MERN Development', description: 'I am a good webdeveloper', imgUrl: Images.about04}
+];
+
 const About = () => {
-  const [abouts, setAbouts] = useState([]);
+    return(
+        <>
+            <h2 className='head-text'>
+                I know that <span>Good Development</span><br />
+                means <span>Good Business</span>
+            </h2>
 
-  useEffect(() => {
-    const query = '*[_type == "abouts"]';
+            <div className="app__profiles">
+                {abouts.map((about, index) => (
+                    <motion.div
+                        whileInView={{opacity:1}}
+                        whileHover={{scale:1.1}}
+                        transition={{duration: 0.5, type: 'tween'}}
+                        className='app__profile-item'
+                        key={about.title + index}>
+                            <img src={about.imgUrl} alt={about.title} />
+                            <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
+                            <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
+                    </motion.div>
+                ))}
+            </div>
 
-    client.fetch(query).then((data) => {
-      setAbouts(data);
-    });
-  }, []);
 
-  return (
-    <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+        </>
+    )
+}; 
 
-      <div className="app__profiles">
-        {abouts.map((about, index) => (
-          <motion.div
-            whileInView={{ opacity: 1 }}
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.5, type: 'tween' }}
-            className="app__profile-item"
-            key={about.title + index}
-          >
-            <img src={urlFor(about.imgUrl)} alt={about.title} />
-            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
-            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
-          </motion.div>
-        ))}
-      </div>
-    </>
-  );
-};
-
-export default AppWrap(
-  MotionWrap(About, 'app__about'),
-  'about',
-  'app__whitebg',
-);
+export default About; 
